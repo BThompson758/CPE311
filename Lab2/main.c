905352d9ccf8cae2df45f082e410eec12c48da73
Robert <bobby.bsa758@gmail.com>
2022-01-27 13:58:10 -0500

    Initial Commit

---
+++ Lab2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab2
+//
+//  Created by Bobby Thompson on 1/27/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
